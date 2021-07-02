d2a20824f9bcbd7777d476fba03adf199229a20b
prakash bohara <prakash.b2055@gmail.com>
2021-07-02 23:59:57 +0545

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+//For opening mobile navbar--
+function hamberger() {
+    document.querySelector('.mobile-nav').classList.add('open');
+}
+
+function times() {
+    document.querySelector('.mobile-nav').classList.remove('open');
+}
